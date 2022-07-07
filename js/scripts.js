cdd158bbd476100a55aaa24f6ccd21fc74cbdcfa
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-06 19:18:54 -0700

    created helper functions and promise.all creates an error in console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
 //Global constants created 
-const searchContainer = document.getElementsByClassName('search-container')
-const gallery = document.getElementById('gallery')//select gallery markup 
-const url = 'https://randomuser.me/api/?results=12&inc=picture,name,email,location,&nat=us'
-let employees = [];
+const searchContainer = document.getElementsByClassName('search-container');
+const gallery = document.getElementById('gallery');//select gallery markup 
+const url = 'https://randomuser.me/api/?results=12&inc=picture,name,email,location,&nat=US';
+let users = [];
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
-//create a function that displays information for 12 people tha include their image, first+last name, email, and location
-//create a fetch  to generate the data needed for each of the 12 profiles 
-fetch (url)
-    .then(res=> res.json())
-    .then(data => console.log(data.results));
+//Promise fetch data to load 
+Promise.all([fetchData(url)])
+    .then(data => {
+        users = data[0].results
+        usersToDisplay(user)
+  });
+  console.log(Error.response)
 
-//Promise fetch data to load ???
-//   Promise.all([
-//     fetchData(url)
-//   ])
+////create a fetch function to generate the data needed for each of the 12 profiles 
+function fetchData(url){
+    return fetch (url)
+        .then (checkStatus)
+        .then(res=> res.json())
+        // .then(data => console.log(data.results))
+        .then(err => console.log("There was an error!", err))
+};
+
+//response to check url
+function checkStatus(response) {
+    if (response.ok) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  }
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
-function usersToDisplay (user){
+//create a function that displays information for 12 people tha include their image, first+last name, email, and location
+function usersToDisplay(user){
 gallery.innerHTML = '';
-
 for (let i=0; i<gallery.length; i++) {
     let html = 
     `<div class="card">
@@ -35,10 +50,9 @@ for (let i=0; i<gallery.length; i++) {
         <p class="card-text">${user[i].email}</p>
         <p class="card-text cap">${user[i].location.city}</p>
     </div>`; 
-}
 
-html.insertAdjacentHTML('beforeend', 'HTML string')
-}
+gallery.insertAdjacentHTML('beforeend', 'html')
+}}
 
 
   
