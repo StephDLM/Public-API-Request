7c81756ab6c896770a8ae80ad078c5db9f359b7e
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-08 10:23:02 -0700

    index is not defined, cannot get the module to pop up when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ const url = 'https://randomuser.me/api/?results=12&inc=picture,name,email,locati
 let users = [];
 const modalContainer = document.getElementsByClassName("modal-container");
 const button = document.querySelector("button");
+const div = document.querySelectorAll("card-img-container")
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -13,7 +14,8 @@ const button = document.querySelector("button");
 Promise.all([fetchData(url)])
     .then(data => {
         users = data[0].results
-        usersToDisplay(users)
+        usersToDisplay(users);
+        userCards(index);
   });
 //   console.log(Error.response)
 
@@ -43,7 +45,7 @@ function usersToDisplay(users){
 gallery.innerHTML = '';
 for (let i=0; i<users.length; i++) {
     let html = 
-    `<div class="card">
+    `<div class="card" >
     <div class="card-img-container">
         <img class="card-img" src="${users[i].picture.medium}" alt="profile picture">
     </div>
@@ -59,57 +61,52 @@ gallery.insertAdjacentHTML('beforeend', html)
 // ------------------------------------------
 //  Module
 // ------------------------------------------
-//idk Store the fetched employee data in a global variable  
-// Create a function to show the modal that accepts an index
+// Store the fetched employee data in a global variable  
 // With that index you can grab the data of the clicked employee (for example employees[index]
-// Use that employee object to generate the HTML for the Modal
-// Append that modal HTML to the body of the document
+gallery.addEventListener("click", (e) =>{
+  if (e.target !== gallery ){
+   let card = e.target.closest(".card");
+   console.log(card)
+   usersToDisplay(users);
+  }
+})
+  userCards(index);
 
 
+// Create a function to show the modal that accepts an index
+function userCards(index) {
+//create an object that holds employee information to store in an index
+  const { picture, name, email, phone, location : {street, city, state, zip },dob } = users[index];
+  const birthDate = new Date(dob.date);
+  const phoneNumber = phone.replace(/-/, ' ');
+  // for (let i=0; i<users.length; i++) { //create a for loop to iterate through all of the card information
+  // Use that user object to generate the HTML for the Modal
 
-gallery.addEventListener('click', (e) => {
-  // const userInfo = document.getElementById("modal-info-container");
-  e.target(userInfo);
+    let htmlModal = `
+    <div class="modal-container" >
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${picture.large}">
+            <h3 id="name" class="modal-name cap">${name.first} ${name.last}</h3>
+            <p class="modal-text">${email}</p>
+            <p class="modal-text cap">${location.city}</p>
+            <hr>
+            <p class="modal-text">${phone}(</p>
+            <p class="modal-text">${street.number} ${street.name} ${city}, ${state} ${zip}</p>
+            <p class="modal-text">Birthday: ${birthDate.getMonth()}/${birthDate.getDate()}/${birthDate.getFullYear()}</p>
+        </div>
+    </div>`;
+}
+
+// Append the modal HTML to the body of the document
+document.body.insertAdjacentHTML('beforeend', htmlModal) //insert in the html to the document body 
+
+//close button 
+const closeModel = document.getElementById("modal-close-btn"); //select the close modal id 
+closeModel.addEventListener("click", e => { //add event listener to close modal 
+ modalContainer.style.display = "none";
 });
 
 
-// ------------------------------------------
-//  POST DATA
-// ------------------------------------------
-function userCards(users) {
-  for (let i=0; i<users.length; i++)
-  var htmlModal = `
-<div class="modal-container">
-<div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-        <img class="modal-img" src="${users[i].picture.large}">
-        <h3 id="name" class="modal-name cap">${users[i].name.first} ${users[i].name.last}</h3>
-        <p class="modal-text">${users[i].email}</p>
-        <p class="modal-text cap">${users[i].location.city}</p>
-        <hr>
-        <p class="modal-text">${users[i]}(555) 555-5555</p>
-        <p class="modal-text">${users[i].street.number} 
-                              ${users[i].street} 
-                              ${users[i].city}, 
-                              ${users[i].state} 
-                              ${users[i].zip}</p>
-        <p class="modal-text">Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
-    </div>
-</div>`;
-//insert in the document body 
-document.body.insertAdjacentHTML('beforeend', htmlModal) //insert in the document body 
-
-}
-
-
-//closer button 
- const closeModel = document.getElementById("modal-close-btn");//select the close modal id 
- //add event listener to close modal 
- closeModel.addEventListener("click", e => {
-  modalContainer.innerHTML = '';
-  modalContainer.classList.add("hide");
- });
-
-  
-
+ 
